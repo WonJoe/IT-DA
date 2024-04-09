458097854b3c23d7b33cd392f2b68c3ba07fcf12
wonjoe <choiwj1995@gmail.com>
2024-04-09 14:51:55 +0900

    test1 add location controller

--- backend/src/main/java/com/itda/location/LocationController.java
+++ backend/src/main/java/com/itda/location/LocationController.java
@@ -1,5 +1,19 @@
 package com.itda.location;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LocationController {
     
+    @CrossOrigin
+	@PostMapping("/test")
+	public ResponseEntity<?> save(@RequestBody Location location) {
+		return new ResponseEntity<>(postService.save(location), HttpStatus.CREATED);
+	}
+
 }

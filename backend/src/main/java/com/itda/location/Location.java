18cb58e370d9ef196fe4c948b84b033522d127d8
wonjoe <choiwj1995@gmail.com>
2024-04-09 13:44:46 +0900

    0409test

---
+++ backend/src/main/java/com/itda/location/Location.java
@@ -0,0 +1,16 @@
+package com.itda.location;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+import lombok.Data;
+
+@Data
+@Table(name = "Location")
+@Entity
+public class Location {
+
+    
+    
+}
